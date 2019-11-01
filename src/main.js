a8bc7f61248ae479c36ffb1b9a8ba458a2c0b055
Thiago Abreu Rodrigues <thiago.ar17@gmail.com>
2019-11-01 19:55:41 -0300

    improvement(applications): change to use dropdown by bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { DropdownPlugin } from 'bootstrap-vue'
+Vue.use(DropdownPlugin)
 
 Vue.config.productionTip = false
 
--- src/views/Applications.vue
+++ src/views/Applications.vue
@@ -1,44 +1,32 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-md-12 py-4">
-        <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #fff">
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-              <li class="nav-item active dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  Selecione uma categoria
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <router-link
-                    class="dropdown-item"
-                    :to="`/apps/`">
-                      Todas
-                  </router-link>
-                  <router-link
-                    class="dropdown-item"
-                    :to="`/apps/${category}`"
-                    :key="category"
-                    v-for="category of categories">
-                      {{category}}
-                  </router-link>
-                </div>
-              </li>
-            </ul>
-            <form class="form-inline my-2 my-lg-0">
-              <input class="form-control mr-sm-2" type="search" placeholder="Pesquisar..." aria-label="Search" v-model="searchField">
-              <button class="btn btn-outline-success my-2 my-sm-0 btn-primary" @click="loadApplications()">Buscar</button>
-            </form>
-          </div>
-        </nav>
+  <div class="container py-4">
+    <div class="d-flex justify-content-between align-items-center bg-white mb-3">
+      <b-dropdown text="Selecione uma categoria" class="m-md-2" variant="light">
+        <b-dropdown-item to="/apps/">Todas</b-dropdown-item>
+        <b-dropdown-item
+          :to="`/apps/${category}`"
+          :key="category"
+          v-for="category of categories"
+        >{{category}}</b-dropdown-item>
+      </b-dropdown>
+      <div class="mr-20">
+        <form class="form-inline my-2 my-lg-0">
+          <input
+            class="form-control mr-sm-2"
+            type="search"
+            placeholder="Pesquisar..."
+            aria-label="Search"
+            v-model="searchField"
+          />
+          <button
+            class="btn btn-outline-success my-2 my-sm-0 btn-primary"
+            @click="loadApplications()"
+          >Buscar</button>
+        </form>
       </div>
     </div>
     <div class="row">
-      <div
-        class="col-md-6 col-lg-6 py-4"
+      <div class="col-md-6 col-lg-6 py-4"
         :key="application.id"
         v-for="application of applications">
           <ec-application-card :application="application"/>
@@ -48,7 +36,7 @@
 </template>
 
 <script>
-import EcApplicationCard from '../components/EcApplicationCard'
+import EcApplicationCard from "../components/EcApplicationCard";
 
 export default {
   name: "Applications",
@@ -59,32 +47,39 @@ export default {
     return {
       applications: [],
       categories: [],
-      searchField: ''
-    }
+      searchField: ""
+    };
   },
   created() {
-    this.loadApplications()
+    this.loadApplications();
   },
   watch: {
-    '$route': 'loadApplications'
+    $route: "loadApplications"
   },
   methods: {
     loadApplications() {
-      let query = this.$route.params.category ? `category=${this.$route.params.category}` : ''
-      query += this.$route.params.category ? `&title=${this.searchField}` : `title=${this.searchField}`
-      fetch(`https://market.e-com.plus/v1/applications/?${query}`)
-        .then(response => {
+      let query = this.$route.params.category
+        ? `category=${this.$route.params.category}`
+        : "";
+      query += this.$route.params.category
+        ? `&title=${this.searchField}`
+        : `title=${this.searchField}`;
+      fetch(`https://market.e-com.plus/v1/applications/?${query}`).then(
+        response => {
           response.json().then(result => {
-            this.applications = result.result
+            this.applications = result.result;
             if (!this.categories.length) {
               this.loadCategories();
             }
-          })
-        })
+          });
+        }
+      );
     },
     loadCategories(applications) {
-      this.categories = [...new Set(this.applications.map(application => application.category))];
-    },
+      this.categories = [
+        ...new Set(this.applications.map(application => application.category))
+      ];
+    }
   }
-}
+};
 </script>
