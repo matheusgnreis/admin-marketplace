cb21ca777df7367f2ef4ad8467f1fa3171988807
Leonardo Matos <leomp120894@gmail.com>
2019-11-17 02:10:54 -0300

    build(webpack): custom webpack config on vue cli config file

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,38 @@
+const devMode = process.env.NODE_ENV !== 'production'
+
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  devServer: {
+    port: 9330,
+    open: true
+  },
+
+  css: {
+    extract: false
+  },
+  configureWebpack: {
+    optimization: {
+      splitChunks: false
+    },
+    output: {
+      library: '__ecomplus_marketplace',
+      libraryTarget: 'umd'
+    }
+  },
+
+  publicPath: devMode ? '/apps/' : '/assets/vendor/ecomplus-marketplace/',
+  outputDir: devMode ? 'test' : 'dist',
+  filenameHashing: false,
+
+  chainWebpack: config => {
+    config.externals([
+      {
+        '@ecomplus/auth': {
+          commonjs: '@ecomplus/auth',
+          commonjs2: '@ecomplus/auth',
+          root: 'EcomAuth'
+        }
+      }
+    ])
+  }
 }
