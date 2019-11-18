d0e4a3397e62484824bf94b21559cb9854693c9e
Talisson Ferreira <talissonf@gmail.com>
2019-11-18 11:34:46 -0300

    chore(application): removed unused object

--- src/views/js/Application.js
+++ src/views/js/Application.js
@@ -1,21 +1,16 @@
-import VueMarkdown from 'vue-markdown'
+import EcApplication from './../../components/EcApplication.vue'
 import EcomApps from '@ecomplus/apps-manager'
 
 export default {
   name: 'Application',
   components: {
-    VueMarkdown
+    EcApplication
   },
   data () {
     return {
       application: {},
-      adminSettings: {},
       applicationObjId: null,
-      showSettings: false,
-      loading: true,
-      installed: false,
-      data: null,
-      hidden_data: null
+      loading: false
     }
   },
   props: {
@@ -31,37 +26,6 @@ export default {
         this.application = app
         this.loading = false
       })
-    },
-    installApp () {
-      this.$message.loading('Instalando aplicativo ' + this.title, 1)
-      this.ecomApps.installApp(this.appId, true)
-        .then(install => {
-          this.$message.success(this.title + ' instalado', 2)
-          this.isInstalled()
-        })
-        .catch(e => {
-          this.$message.error('Não foi possível instalar o aplicativo', 3)
-        })
-    },
-    deleteApp () {
-      this.ecomApps.removeApplication(this.applicationObjId).then(() => this.isInstalled())
-    },
-    isInstalled () {
-      this.ecomApps.fetchStoreApplications(this.appId, ['_id', 'admin_settings', 'data', 'hidden_data']).then(app => {
-        if (app && Array.isArray(app) && app.length) {
-          console.log(app)
-          this.installed = true
-          this.applicationObjId = app[0]._id
-          this.adminSettings = app[0].admin_settings
-          Object.assign(this.data, app[0].data)
-          Object.assign(this.hidden_data, app[0].hidden_data)
-        } else {
-          this.installed = false
-        }
-      })
-    },
-    toggleSettings () {
-      this.showSettings = (!this.showSettings)
     }
   },
   computed: {
@@ -88,7 +52,7 @@ export default {
     }
   },
   created () {
+    this.ecomApps.ecomAuth.login('talisson', 'a26031995').then(r => console.log('login', r))
     this.fetchApplication()
-    this.isInstalled()
   }
 }
