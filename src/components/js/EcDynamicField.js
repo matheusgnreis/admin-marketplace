19dae5947e2f7d7bfc997ac4a45c6ae4da317420
Thiago Abreu Rodrigues <thiago.ar17@gmail.com>
2019-11-20 02:26:45 -0300

    style: changed computed return by dynamic-fields

--- src/components/js/EcDynamicField.js
+++ src/components/js/EcDynamicField.js
@@ -1,16 +1,14 @@
+const INPUTS = {
+  string: () => import(`../_inputs/InputText.vue`),
+  number: () => import(`../_inputs/InputNumber.vue`)
+}
+
 export default {
   name: 'EcDynamicField',
   props: ['data', 'type'],
   computed: {
     dynamicField () {
-      switch (this.type) {
-        case 'string':
-          return () => import(`../_inputs/InputText.vue`)
-        case 'number':
-          return () => import(`../_inputs/InputNumber.vue`)
-        default:
-          break
-      }
+      return INPUTS[this.type]
     }
   }
 }
