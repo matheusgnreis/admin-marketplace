6d48e44601edcad7c2e665735588322ad89e26f1
Leonardo Matos <leomp120894@gmail.com>
2019-11-26 19:44:09 -0300

    fix(app-card): update app card component to new porposed view/styles

--- src/components/EcAppCard.vue
+++ src/components/EcAppCard.vue
@@ -1,3 +1,2 @@
 <script src="./js/EcAppCard.js"></script>
 <template lang="html" src="./html/EcAppCard.html"></template>
-<style lang="scss" src="./scss/EcAppCard.scss"></style>
--- src/components/js/EcAppCard.js
+++ src/components/js/EcAppCard.js
@@ -1,37 +1,53 @@
 import { i18n } from '@ecomplus/utils'
-import { i19install, i19edit } from '@ecomplus/i18n'
 
-const MAX_LENGHT_DESCRIPTION = 100
+import {
+  i19free
+  // i19paid
+} from '@ecomplus/i18n'
 
 export default {
   name: 'EcAppCard',
+
   props: {
     app: {
       type: Object,
       default: {}
     },
-    isInstalled: {
-      type: Boolean
+    descriptionMaxLength: {
+      type: Number,
+      default: 95
     }
   },
+
   computed: {
-    i19edit () {
-      return i18n(i19edit)
+    i19free () {
+      return i18n(i19free)
     },
-    i19install () {
-      return i18n(i19install)
+
+    i19paid () {
+      return 'Pago'
     },
-    appUrlIcon () {
-      if (!this.app.icon.startsWith('https://')) {
-        return `https://market.e-com.plus${this.app.icon}`
+
+    iconUrl () {
+      if (this.app.icon) {
+        if (!this.app.icon.startsWith('https://')) {
+          return `https://market.e-com.plus${this.app.icon}`
+        }
+        return this.app.icon
       }
-      return this.app.icon
+      return ''
     },
+
     formattedDescription () {
-      if (this.app.short_description.length > MAX_LENGHT_DESCRIPTION) {
-        return `${this.app.short_description.slice(0, MAX_LENGHT_DESCRIPTION)}...`
+      if (this.app.short_description) {
+        const { descriptionMaxLength } = this
+        if (this.app.short_description.length > descriptionMaxLength) {
+          return `${this.app.short_description.slice(0, descriptionMaxLength)}...`
+        }
+        return this.app.short_description
+      } else {
+        return ''
       }
-      return this.app.short_description
     }
   }
 }
