6b7871830ad6c13b774f93f996e18cb28ad6ca84
Leonardo Matos <leomp120894@gmail.com>
2019-12-06 13:38:06 -0300

    fix(dynamic-field): using v-model, fix component api

--- src/components/js/EcDynamicField.js
+++ src/components/js/EcDynamicField.js
@@ -2,18 +2,23 @@ import { discover } from '../../utils/Discoverer.js'
 
 export default {
   name: 'EcDynamicField',
+
   props: {
-    field: {
-      type: String
-    },
-    schema: {
-      type: Object
-    },
-    value: {
-      type: [String, Number, Boolean, Object]
-    }
+    field: String,
+    schema: Object,
+    value: [String, Number, Boolean, Object]
   },
+
   computed: {
+    localValue: {
+      get () {
+        return this.value
+      },
+      set (value) {
+        this.$emit('input', value)
+      }
+    },
+
     dynamicFields () {
       return discover(this.field, this.schema)
     }
