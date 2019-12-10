a0f717f55e8b2087069975bb038cabb5b14e54e0
Thiago Abreu Rodrigues <thiago.ar17@gmail.com>
2019-12-09 21:04:21 -0300

    feat(input-boolean): implemented input-boolean

---
+++ src/components/_inputs/InputBoolean.vue
@@ -0,0 +1,11 @@
+<template>
+    <a-form-item :label="schema.title">
+      <a-switch
+        :name="name"
+        :value="value"
+        v-model="localValue"
+      />
+    </a-form-item>
+  </template>
+
+  <script src="./js/InputBoolean.js"></script>
