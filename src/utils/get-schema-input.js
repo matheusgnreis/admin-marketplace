742b31216d21285721c2785a6708017377741dc4
Leonardo Matos <leomp120894@gmail.com>
2019-12-16 13:09:00 -0300

    refactor(get-schema-input): changing discover function approach

--- src/utils/get-schema-input.js
+++ src/utils/get-schema-input.js
@@ -1,5 +1,4 @@
 import InputDiscount from '../components/_inputs/InputDiscount.vue'
-import InputObject from '../components/_inputs/InputObject.vue'
 import InputMoney from '../components/_inputs/InputMoney.vue'
 import InputZipCode from '../components/_inputs/InputZipCode.vue'
 import Upload from '../components/_inputs/Upload.vue'
@@ -10,7 +9,7 @@ import InputBoolean from '../components/_inputs/InputBoolean.vue'
 
 const isDiscount = (field, schema) => {
   if (field.includes('discount') && schema.type === 'object') {
-    if (schema.properties.hasOwnProperty('value') && schema.properties.hasOwnProperty('type')) {
+    if (schema.properties.value && schema.properties.type) {
       return isNumber('value', schema.properties.value) && isEnum('type', schema.properties.type)
     }
   }
@@ -29,12 +28,8 @@ const isUpload = (field, schema) => {
   return field.includes('icon') && schema.format === 'uri'
 }
 
-const isObject = (field, schema) => {
-  return schema.type === 'object' && !isDiscount(field, schema)
-}
-
 const isEnum = (field, schema) => {
-  return schema.type === 'string' && schema.hasOwnProperty('enum')
+  return schema.type === 'string' && schema.enum
 }
 
 const isNumber = (field, schema) => {
@@ -49,9 +44,8 @@ const isBoolean = (field, schema) => {
   return schema.type === 'boolean'
 }
 
-const INPUTS = [
+const inputRules = [
   { match: isDiscount, component: InputDiscount },
-  { match: isObject, component: InputObject },
   { match: isMoney, component: InputMoney },
   { match: isZipCode, component: InputZipCode },
   { match: isUpload, component: Upload },
@@ -61,10 +55,10 @@ const INPUTS = [
   { match: isBoolean, component: InputBoolean }
 ]
 
-export const discover = (field, schema) => {
-  for (let input of INPUTS) {
-    if (input.match(field, schema)) {
-      return [input.component]
+export default (field, schema) => {
+  for (const { match, component } of inputRules) {
+    if (match(field, schema)) {
+      return component
     }
   }
 }
