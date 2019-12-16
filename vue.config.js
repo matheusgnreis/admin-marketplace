2afd259d32232357c2a8f244cc3022ce52628a4c
Leonardo Matos <leomp120894@gmail.com>
2019-12-16 17:25:51 -0300

    build(webpack): update external libs (from cdn)

--- vue.config.js
+++ vue.config.js
@@ -27,6 +27,41 @@ module.exports = {
   chainWebpack: config => {
     config.externals([
       {
+        axios: {
+          commonjs: 'axios',
+          commonjs2: 'axios',
+          root: 'axios'
+        }
+      },
+      {
+        eventemitter3: {
+          commonjs: 'eventemitter3',
+          commonjs2: 'eventemitter3',
+          root: 'EventEmitter3'
+        }
+      },
+      {
+        'blueimp-md5': {
+          commonjs: 'blueimp-md5',
+          commonjs2: 'blueimp-md5',
+          root: 'md5'
+        }
+      },
+      {
+        '@ecomplus/utils': {
+          commonjs: '@ecomplus/utils',
+          commonjs2: '@ecomplus/utils',
+          root: 'ecomUtils'
+        }
+      },
+      {
+        '@ecomplus/client': {
+          commonjs: '@ecomplus/client',
+          commonjs2: '@ecomplus/client',
+          root: 'ecomClient'
+        }
+      },
+      {
         '@ecomplus/auth': {
           commonjs: '@ecomplus/auth',
           commonjs2: '@ecomplus/auth',
