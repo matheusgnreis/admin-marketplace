85d79f09ee8d3378c2e89dc4cfc145061c2422e9
Leonardo Matos <leomp120894@gmail.com>
2019-12-17 11:55:16 -0300

    Update src/components/js/EcApplication.js

--- src/components/js/EcApplication.js
+++ src/components/js/EcApplication.js
@@ -117,7 +117,7 @@ export default {
     },
 
     randomColors () {
-      return '#'+(Math.random()*0xFFFFFF<<0).toString(16)
+      return '#' + (Math.random() * 0xFFFFFF << 0).toString(16)
     },
 
     price () {
