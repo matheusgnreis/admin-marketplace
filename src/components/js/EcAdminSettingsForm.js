671e1d5aabe5bfbb9023bcd1fb8bae716a2ad566
Leonardo Matos <leomp120894@gmail.com>
2019-12-17 00:41:47 -0300

    feat(admin-settings): handling array of nested objects on form

--- src/components/js/EcAdminSettingsForm.js
+++ src/components/js/EcAdminSettingsForm.js
@@ -2,6 +2,10 @@ import { i18n } from '@ecomplus/utils'
 import getSchemaInput from './../../lib/get-schema-input'
 
 import {
+  i19add,
+  i19edit,
+  // i19empty,
+  // i19editing,
   // i19general,
   i19save
 } from '@ecomplus/i18n'
@@ -19,11 +23,16 @@ export default {
   data () {
     return {
       data: {},
-      hiddenData: {}
+      hiddenData: {},
+      dataListsIndexes: {}
     }
   },
 
   computed: {
+    i19add: () => i18n(i19add),
+    i19edit: () => i18n(i19edit),
+    i19editing: () => 'Editando',
+    i19empty: () => 'Vazio',
     i19general: () => 'Geral',
     i19save: () => i18n(i19save),
 
@@ -61,6 +70,10 @@ export default {
       return schema.type === 'object' || schema.type === 'array'
     },
 
+    checkNestedObjectsArray (schema) {
+      return schema.type === 'array' && schema.items && schema.items.type === 'object'
+    },
+
     getDescriptionHtml (description) {
       return description.replace(
         /(http(s)?:\/\/[^\s]+)/g,
@@ -68,18 +81,49 @@ export default {
       )
     },
 
-    parseAdminSettingsField ({ field, schema, hide, data, parent = '' }) {
+    getObjectValues (obj) {
+      let str = ''
+      for (const prop in obj) {
+        if (obj[prop]) {
+          switch (typeof obj[prop]) {
+            case 'number':
+            case 'string':
+              str += `${obj[prop].toString()} / `
+          }
+        }
+      }
+      return str.length > 3 ? str : `${this.i19empty} ...`
+    },
+
+    parseAdminSettingsField ({ field, schema, hide, data, parentFields = '' }) {
       if (!data) {
         data = hide ? this.hiddenData : this.data
       }
       let fieldObjects = []
-      const { localSchema, component } = getSchemaInput(field, schema)
+      let refSchema
+      if (this.checkNestedObjectsArray(schema)) {
+        if (!data[field] || !data[field].length) {
+          if (parentFields === '') {
+            this.$set(data, field, [{}])
+            this.$set(this.dataListsIndexes, field, 0)
+          } else {
+            data[field] = [{}]
+          }
+        }
+        data = data[field]
+        refSchema = schema.items
+        field = this.dataListsIndexes[field] || 0
+      } else {
+        refSchema = schema
+      }
+      const { localSchema, component } = getSchemaInput(field, refSchema)
+      parentFields = `${parentFields}.${field}`
       if (component) {
         fieldObjects.push({
           field,
           schema,
           data,
-          name: `${parent}.${field}`,
+          name: parentFields,
           component
         })
       }
@@ -96,7 +140,7 @@ export default {
                 field: nestedField,
                 schema: childSchema,
                 data: data[field],
-                parent: `${parent}.${nestedField}`
+                parentFields: `${parentFields}.${nestedField}`
               })
             )
           }
